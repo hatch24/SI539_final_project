021d1992491f91663050ca024b9dd5fa7d6bdf19
hatch24 <156356671+hatch24@users.noreply.github.com>
2024-04-17 17:44:04 -0400

    Add files via upload

--- js/minigame.js
+++ js/minigame.js
@@ -1,14 +1,40 @@
-function allowDrop(ev) {
-	ev.preventDefault();
-  }
+const source1 = document.querySelector(".source1");
+const source2 = document.querySelector(".source2");
+const source3 = document.querySelector(".source3");
 
-  function drag(ev) {
-	ev.dataTransfer.setData("text", ev.target.id);
-  }
+const div1 = document.querySelector(".target");
 
-  function drop(ev) {
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
-	ev.target.appendChild(document.getElementById(data));
-  }
+source1.addEventListener("dragstart", function(e){
+	e.dataTransfer.setData("text/plain", e.target.id);
+})
 
+source2.addEventListener("dragstart", function(e){
+	e.dataTransfer.setData("text/plain", e.target.id);
+})
+
+source3.addEventListener("dragstart", function(e){
+	e.dataTransfer.setData("text/plain", e.target.id);
+})
+
+
+target.addEventListener("dragover", function(e){
+	e.preventDefault();
+})
+
+target.addEventListener("drop", function(e){
+	e.preventDefault();
+	const source1ID = e.dataTransfer.getData("text/plain");
+	e.target.appendChild(document.getElementById(source1ID));
+})
+
+target.addEventListener("drop", function(e){
+	e.preventDefault();
+	const source2ID = e.dataTransfer.getData("text/plain");
+	e.target.appendChild(document.getElementById(source2ID));
+})
+
+target.addEventListener("drop", function(e){
+	e.preventDefault();
+	const source3ID = e.dataTransfer.getData("text/plain");
+	e.target.appendChild(document.getElementById(source3ID));
+})
